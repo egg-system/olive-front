6adc1557ca568eda3c14a687b5a1cd6ed275121e
hikarut <ht.hikaru.takahashi@gmail.com>
2018-12-26 08:04:18 +0900

    mod default component

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,10 @@
     <div>
       <logo/>
       <h1 class="title">
-        olive
+        olive salon
       </h1>
-      <h2 class="subtitle">
-        My peachy Nuxt.js project
-      </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-        <div class="links">
-          <nuxt-link to="/menu">メニュー</nuxt-link>
-        </div>
+        <nuxt-link to="/menu">メニュー</nuxt-link>
       </div>
     </div>
     <Footer />
