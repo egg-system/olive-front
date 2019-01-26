95c4579d5197538283f85781ee318032c8b1d079
hikarut <ht.hikaru.takahashi@gmail.com>
2019-01-26 19:37:01 +0900

    change to table

--- pages/registration/Menu.vue
+++ pages/registration/Menu.vue
@@ -9,37 +9,27 @@
       </v-flex>
     </v-layout>
 
-    <v-layout row align-self-baseline>
+    <v-layout row>
       <v-flex>
-        <v-chip label>
-          <v-icon left>label</v-icon>整体・マッサージ
-        </v-chip>
-      </v-flex>
-      <v-flex xs3>
-        <div>通常整体コース</div>
-      </v-flex>
-      <v-flex xs3>
-        <div>¥6,000（税抜）</div>
-      </v-flex>
-      <v-flex xs3>
-        <div>60分</div>
-      </v-flex>
-    </v-layout>
-
-    <v-layout row align-self-baseline>
-      <v-flex>
-        <v-chip label>
-          <v-icon left>label</v-icon>整体・マッサージ
-        </v-chip>
-      </v-flex>
-      <v-flex xs3>
-        <div>足つぼ</div>
-      </v-flex>
-      <v-flex xs3>
-        <div>¥2,000（税抜）</div>
-      </v-flex>
-      <v-flex xs3>
-        <div>0分</div>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          hide-actions
+          hide-headers
+          class="elevation-1"
+        >
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.course }}</td>
+            <td class="text-xs-right">{{ props.item.price }}</td>
+            <td class="text-xs-right">{{ props.item.time }}</td>
+          </template>
+          <template slot="footer">
+            <td :colspan="headers.length">
+              <strong>合計</strong>
+            </td>
+          </template>
+        </v-data-table>
       </v-flex>
     </v-layout>
 
@@ -47,11 +37,49 @@
 
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: 'メニュー',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'コース', value: 'course' },
+        { text: '金額', value: 'price' },
+        { text: '所要時間', value: 'time' }
+      ],
+      desserts: [
+        {
+          name: '整体・マッサージ',
+          course: '通常整体コース',
+          price: '¥6,000（税抜）',
+          time: '60分'
+        },
+        {
+          name: '整体・マッサージ',
+          course: '足つぼ',
+          price: '¥2,000（税抜）',
+          time: '0分'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
 <style>
 .menu {
   text-align: left;
 }
 .v-card__text {
   padding: 7px;
+}
+table.v-table tbody td:first-child {
+  padding: 0px 10px;
 }
 </style>
