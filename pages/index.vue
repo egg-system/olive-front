fe0c74433e18fe65cb7cd94666ebf79505c45dc3
hikarut <ht.hikaru.takahashi@gmail.com>
2019-01-27 09:10:52 +0900

    for 0131 check

--- pages/index.vue
+++ pages/index.vue
@@ -2,79 +2,23 @@
   <section class="container">
     <div>
       <logo/>
-      <h1 class="mainTitle">
-        olive salon
-      </h1>
-      <div class="text-xs-center">
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/registration">
-            /registration
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/option">
-            /option
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/menu">
-            /menu
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/login">
-            /login
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/date">
-            /date
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/confirm">
-            /confirm
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>link</v-icon>
-          <nuxt-link to="/complete">
-            /complete
-          </nuxt-link>
-        </v-chip>
-      </div>
+      <h2>
+        オリーヴボディーケア予約<br>ログインしてご予約ください。
+      </h2>
+      <Form />
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Form from '~/pages/login/Form.vue'
 
 export default {
   components: {
-    Logo
+    Form
   }
 }
 </script>
 
 <style>
-.mainTitle {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 50px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
