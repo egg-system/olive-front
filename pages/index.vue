21f968b27616fa9d16f3c57ed6ce8acf3ba125a5
hikarut <ht.hikaru.takahashi@gmail.com>
2019-01-29 21:45:44 +0900

    add link

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@
         オリーヴボディーケア予約<br>ログインしてご予約ください。
       </h2>
       <Form />
+      <a href="https://olivebodycare.jp/web-reservation/email/">
+        新規会員登録をご希望の方はこちらから
+      </a>
     </div>
   </section>
 </template>
