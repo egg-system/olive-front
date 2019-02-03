8bc86e344969b7bc9d24ee7e1c2643e92215d5a0
hikarut <ht.hikaru.takahashi@gmail.com>
2019-02-04 08:35:48 +0900

    add confirm date

--- components/pages/common/RegistrationMenu.vue
+++ components/pages/common/RegistrationMenu.vue
@@ -9,6 +9,15 @@
       </v-flex>
     </v-layout>
 
+    <v-layout v-if="isConfirm" row>
+      <v-flex>
+        <v-card-text>来店日時</v-card-text>
+      </v-flex>
+      <v-flex>
+        <v-card-text>2019年1月21日（月）18:00〜</v-card-text>
+      </v-flex>
+    </v-layout>
+
     <v-layout row>
       <v-flex>
         <v-data-table
@@ -26,6 +35,16 @@
         </v-data-table>
       </v-flex>
     </v-layout>
+
+    <v-layout v-if="isConfirm" row>
+      <v-flex>
+        <v-card-text>マイページからキャンセル可能期限</v-card-text>
+      </v-flex>
+      <v-flex>
+        <v-card-text>2019年1月18日（金）23:59</v-card-text>
+      </v-flex>
+    </v-layout>
+    <v-card-text v-if="isConfirm">※それ以降のキャンセルは直接サロンへご連絡ください。</v-card-text>
 
   </div>
 
