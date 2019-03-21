be7d5226fb4ca2db16265bf22ede3e5330e4229b
hikarut <ht.hikaru.takahashi@gmail.com>
2019-03-22 08:33:00 +0900

    マイページの回数券表示

---
+++ components/pages/mypage/Coupon.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>
+      <p class="under">残りの回数券（回数券をお持ちの方のみ)</p>
+    </h3>
+    <v-layout align-center justify-center row>
+      <v-flex d-flex xs12 md4>
+        <v-card color="white">
+          <v-card-title primary class="card-title">施術回数券</v-card-title>
+          <v-card-text>残<p class="number">3</p>枚</v-card-text>
+          <v-card-text>有効期限：2019/9/30</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 md4>
+        <v-card color="white">
+          <v-card-title primary class="card-title">インデプス回数券</v-card-title>
+          <v-card-text>残<p class="number">0</p>枚</v-card-text>
+          <v-card-text>有効期限：2019/9/30</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+
+<style>
+.v-card__title,
+.v-card__text {
+  color: black;
+}
+.card-title {
+  font-size: 15px;
+}
+.number {
+  display: inline;
+  font-size: 30px;
+}
+</style>
--- pages/mypage/top.vue
+++ pages/mypage/top.vue
@@ -3,6 +3,18 @@
     <mypage-header />
     <v-container grid-list-xl>
       <v-layout column>
+        <v-flex align-self-baseline>
+          <div>山田　太郎　さん</div>
+        </v-flex>
+
+        <v-flex class="mypage-top">
+          <v-card dark color="red lighten-2">
+            <v-card-text><h3 class="mypage-title">マイページトップ</h3></v-card-text>
+          </v-card>
+        </v-flex>
+
+        <mypage-coupon />
+
         <div>aaa</div>
         <div>aaa</div>
       </v-layout>
@@ -12,11 +24,13 @@
 
 <script>
 import MypageHeader from '~/components/pages/mypage/Header.vue'
+import MypageCoupon from '~/components/pages/mypage/Coupon.vue'
 import { mapState, mapActions, mapMutations } from 'vuex'
 
 export default {
   components: {
-    MypageHeader
+    MypageHeader,
+    MypageCoupon
   },
   data: () => ({
     link: '/mypage/top'
@@ -38,4 +52,16 @@ export default {
 </script>
 
 <style>
+.mypage-top {
+  text-align: left;
+}
+.mypage-title {
+  color: white;
+}
+.under {
+  background: linear-gradient(transparent 90%, #ff99ab 70%);
+  width: 90%;
+  margin: 0 auto;
+  text-align: left;
+}
 </style>
