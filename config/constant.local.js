92209dd3c55bd1d92ecdf4303b65c5a3eb44d526
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-03-29 11:40:28 +0900

    npmコマンドの追加

---
+++ config/constant.local.js
@@ -0,0 +1,11 @@
+module.exports = {
+  api: {
+    customerLogin: 'https://api.myjson.com/bins/14vniu',
+    customerCreate: 'https://api.myjson.com/bins/18ieba',
+    customerReset: 'https://api.myjson.com/bins/18ieba',
+    reserveCommit: 'https://api.myjson.com/bins/18ieba',
+    menu: `http://localhost:8080/api/shops/:id/menus`,
+    shop: `http://localhost:8080/api/shops/:id`,
+    date: 'http://localhost:8080/api/shops/:id/dates'
+  }
+}
