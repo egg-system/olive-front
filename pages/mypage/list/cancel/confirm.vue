a0fd9763f1e5a9959f083ebafc01784f00efd290
hikarut <ht.hikaru.takahashi@gmail.com>
2019-04-01 08:17:42 +0900

    予約キャンセル確認・完了画面の追加

---
+++ pages/mypage/list/cancel/confirm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <mypage-header />
+    <v-container grid-list-xl>
+      <v-layout column>
+        <v-flex align-self-baseline>
+          <div>山田　太郎　さん</div>
+        </v-flex>
+
+        <v-flex class="mypage-top">
+          <v-card dark color="red lighten-2">
+            <v-card-text><h3 class="mypage-title">マイページトップ > 予約一覧 > 予約キャンセル確認</h3></v-card-text>
+          </v-card>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import MypageHeader from '~/components/pages/mypage/Header.vue'
+import MypageReserveHistory from '~/components/pages/mypage/ReserveHistory.vue'
+
+export default {
+  components: {
+    MypageHeader
+  }
+}
+</script>
+
+<style>
+.mypage-title {
+  color: white;
+}
+</style>
