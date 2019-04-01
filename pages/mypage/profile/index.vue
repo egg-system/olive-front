5714871f17a476ee532c6e0122d9e482adbc36c2
hikarut <ht.hikaru.takahashi@gmail.com>
2019-04-02 08:18:15 +0900

    会員情報ページ作成

---
+++ pages/mypage/profile/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <mypage-header />
+    <v-container grid-list-xl>
+      <v-layout column>
+        <mypage-name />
+
+        <v-flex class="mypage-top">
+          <v-card dark color="red lighten-2">
+            <v-card-text><h3 class="mypage-title">マイページトップ > 会員情報変更</h3></v-card-text>
+          </v-card>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import MypageHeader from '~/components/pages/mypage/Header.vue'
+import MypageReserveHistory from '~/components/pages/mypage/ReserveHistory.vue'
+import MypageListPagination from '~/components/pages/mypage/ListPagination.vue'
+import MypageName from '~/components/pages/mypage/Name.vue'
+import { mapState, mapActions, mapMutations } from 'vuex'
+
+export default {
+  components: {
+    MypageHeader,
+    MypageReserveHistory,
+    MypageListPagination,
+    MypageName
+  },
+  data: () => ({}),
+  computed: {
+    ...mapState({
+      login: state => state.login
+    })
+  }
+}
+</script>
+
+<style>
+.mypage-top {
+  text-align: left;
+}
+.mypage-title {
+  color: white;
+}
+.under {
+  background: linear-gradient(transparent 90%, #ff99ab 70%);
+  width: 90%;
+  margin: 0 auto;
+  text-align: left;
+}
+</style>
