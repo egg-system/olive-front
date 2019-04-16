1fd77e548c4411683f195def9bf7a6aecf87167a
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-04-16 16:09:22 +0900

    fix : カレンダー周りの修正

---
+++ components/pages/date/Calendar/TimeCalendarRow.vue
@@ -0,0 +1,76 @@
+<template>
+  <tr>
+    <td v-if="isPast" class="disabled">-</td>
+    <td v-else-if="canReserveTime" @click="setDataTimeSlot">
+      <a>{{ remainCount | remainFormat }}</a>
+    </td>
+    <td v-else class="disabled">×</td>
+  </tr>
+</template>
+
+<script>
+import _ from 'lodash'
+import moment from 'moment'
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  filter: {
+    remainFormat(value) {
+      if (value <= 0) {
+        return '×'
+      }
+
+      return value < 3 ? value : '○'
+    }
+  },
+  props: {
+    dateTimeSlot: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    canReserveTime() {
+      return this.remainCount > 0
+    },
+    remainCount() {
+      const remains = this.timeSlots.map(timeSlot => timeSlot.remain)
+      return _.min(remains)
+    },
+    timeSlots() {
+      if (!this.dateSlot) {
+        return []
+      }
+
+      return this.dateSlot.filter(timeSlot => {
+        const slotAt = moment(timeSlot.start_time, 'YYYYMMDDHHmm')
+
+        const isSameOrAfter = slotAt.isSameOrAfter(this.dateTimeSlot)
+        const isBefore = slotAt.isBefore(this.slotEndAt)
+        return isSameOrAfter && isBefore
+      })
+    },
+    dateSlot() {
+      return this.getDateSlot(this.dateTimeSlot)
+    },
+    isPast() {
+      return this.dateTimeSlot.isBefore(moment())
+    },
+    nextRoute() {
+      return this.isLogin ? 'login' : 'registration'
+    },
+    slotEndAt() {
+      return this.dateTimeSlot.clone().add(this.timeSlotIncrement, 'hours')
+    },
+    ...mapGetters('login', ['isLogin']),
+    ...mapGetters('date', ['timeSlotIncrement', 'getDateSlot'])
+  },
+  methods: {
+    setDataTimeSlot() {
+      this.setSelectedDateTime(this.dateTimeSlot)
+      this.$router.push({ name: this.nextRoute })
+    },
+    ...mapMutations('select', ['setSelectedDateTime'])
+  }
+}
+</script>
