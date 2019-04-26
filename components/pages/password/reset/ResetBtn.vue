de1fd32f9fb597ca2b71b368c3dcd7d9d7be53de
hikarut <ht.hikaru.takahashi@gmail.com>
2019-04-26 14:24:35 +0900

    ボタンの位置を修正

--- components/pages/password/reset/ResetBtn.vue
+++ components/pages/password/reset/ResetBtn.vue
@@ -2,12 +2,12 @@
   <div>
     <v-layout column>
       <v-flex xs6>
-        <v-btn @click="back">戻る</v-btn>
-      </v-flex>
-      <v-flex xs6>
         <v-btn :disabled="!canClick" color="warning" @click="confirm">
           パスワードをリセットする
         </v-btn>
+      </v-flex>
+      <v-flex xs6>
+        <v-btn @click="back">戻る</v-btn>
       </v-flex>
     </v-layout>
 
