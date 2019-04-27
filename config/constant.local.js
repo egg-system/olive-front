d7dea75a27f87b45ba29cfa2d88d40d351984468
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-04-27 23:49:56 +0900

    fix : 定数の修正

--- config/constant.local.js
+++ config/constant.local.js
@@ -2,7 +2,7 @@ module.exports = {
   api: {
     customerLogin: 'http://localhost:8080/api/customers/sign_in',
     customerCreate: 'http://localhost:8080/api/customers',
-    customerReset: 'https://api.myjson.com/bins/18ieba',
+    customerReset: 'http://localhost:8080/api/customers/password',
     reserveCommit: 'http://localhost:8080/api/reservations',
     menu: `http://localhost:8080/api/shops/:id/menus`,
     shop: `http://localhost:8080/api/shops/:id`,
