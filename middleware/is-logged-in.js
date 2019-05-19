296ce18078061a07116349a35be44d3ebb471c81
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-05-19 17:05:44 +0900

    fix : ミドルウェアのバグ修正

--- middleware/is-logged-in.js
+++ middleware/is-logged-in.js
@@ -1,16 +1,17 @@
 export default ({ store, redirect, route }) => {
   const isLoggedIn = store.getters['login/isLogin']
   switch (route.name) {
-    case 'mypage':
+    case 'mypage-login':
+      if (isLoggedIn) {
+        redirect('/mypage')
+      }
+      break
+
+    default:
       if (!isLoggedIn) {
         redirect('/mypage/login')
       }
       break
 
-    default:
-      if (isLoggedIn) {
-        redirect('/mypage')
-      }
-      break
   }
 }
