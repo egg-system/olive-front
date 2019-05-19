2524152ed85200af122c4a65db6c0656c5f67fc8
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-05-19 22:43:35 +0900

    fix : パスの修正

---
+++ components/pages/mypage/reservations/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-xs-center">
+    <v-pagination
+      v-model="page"
+      :length="totalPages"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    page: {
+      get() {
+        return Number(this.$route.query.page)
+      },
+      set(page) {
+        this.$router.push({
+          name: 'mypage-reservations',
+          query: { page: page, cancel: this.doCancel }
+        })
+      }
+    },
+    ...mapState('reservation', ['doCancel', 'totalPages'])
+  }
+}
+</script>
