a8484c18cfae4f15d79ff8f6bcec80195acf67d2
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-05-19 22:44:42 +0900

    fix : 予約の表示処理の追加

---
+++ components/pages/mypage/reservations/ReserveData.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card color="white">
+    <div class="card-main">
+      <v-layout row>
+        <v-flex xs5 class="text-menu">
+          <v-chip :color="reserveStateCss" label text-color="white">{{ data.state }}</v-chip>
+        </v-flex>
+        <v-flex xs6>
+          <div class="text-value shop">{{ data.store.name }}</div>
+        </v-flex>
+        <v-flex v-if="doCancel && data.state === '予約中'" xs6 >
+          <v-btn class="cancel-btn" color="warning" @click="cancelConfrim(data.id)">キャンセル</v-btn>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs5>
+          <div class="text-menu">予約日時</div>
+        </v-flex>
+        <v-flex xs6>
+          <div class="text-value">{{ data.start_at | dateTimeAndDatFormat }}</div>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs5>
+          <div class="text-menu">予約ID</div>
+        </v-flex>
+        <v-flex xs6>
+          <div class="text-value">{{ data.id }}</div>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs5>
+          <div class="text-menu">メニュー</div>
+        </v-flex>
+        <template v-for="(menu, index) in data.menus">
+          <v-flex :key="index" xs6>
+            <div class="text-value">
+              <span v-if="isMultiReserved">{{ (index + 1) }}時間目</span>
+              {{ menu.name }}
+            </div>
+          </v-flex>
+        </template>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs5>
+          <div class="text-menu">合計金額</div>
+        </v-flex>
+        <v-flex xs6>
+          <div class="text-value">{{ data.fee }}</div>
+        </v-flex>
+      </v-layout>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    },
+    isShownCancelBtn: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    reserveStateCss() {
+      switch (this.data.state) {
+        case '予約中':
+          return 'primary'
+        case 'キャンセル':
+          return 'red'
+        default:
+          return ''
+      }
+    },
+    isMultiReserved() {
+      return this.data.menus.length > 1
+    },
+    ...mapState('reservation', ['doCancel'])
+  },
+  methods: {
+    cancelConfrim(id) {
+      this.$router.push({
+        name: 'mypage-reservations-id-cancel',
+        params: { id }
+      })
+    }
+  }
+}
+</script>
---
+++ components/pages/mypage/reservations/ReserveHistory.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-layout justify-center column>
+    <template v-for="data in reserveData">
+      <v-flex 
+        :key="data.id"
+        d-flex
+        xs12
+        mad4
+        class="card-width"
+      >
+        <reserve-data :data="data"/>
+      </v-flex>
+    </template>
+  </v-layout>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import ReserveData from './ReserveData.vue'
+
+export default {
+  components: { ReserveData },
+  props: {
+    limit: {
+      type: Number,
+      default: null
+    }
+  },
+  computed: {
+    reserveData() {
+      const reserveData = this.reservations.filter(
+        reservation => !this.id || reservation.id === this.id
+      )
+
+      if (this.limit) {
+        return reserveData.slice(0, this.limit)
+      }
+
+      return reserveData
+    },
+    ...mapState('reservation', ['reservations'])
+  }
+}
+</script>
+
+<style>
+.v-card__title,
+.v-card__text {
+  color: black;
+}
+.number {
+  display: inline;
+  font-size: 30px;
+}
+.card-width {
+  width: 90%;
+  margin: 0 auto;
+}
+.shop {
+  margin-top: 12px;
+}
+.text-menu {
+  text-align: right;
+}
+.text-value {
+  text-align: left;
+}
+.card-main {
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+.cancel-btn {
+  width: 10px;
+}
+</style>
