8fbb16f1b4e7fbf617b3cea0954b4e64795cb651
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-06-24 17:53:53 +0900

    fix : 文言の変更

--- components/pages/common/PasswordAlert.vue
+++ components/pages/common/PasswordAlert.vue
@@ -2,5 +2,9 @@
   <v-alert :value="true" type="error" outline>
     システムのリニューアルに伴い、以前ユーザー登録をした方はこちらからパスワードの再設定をお願いいたします。<br>
     <nuxt-link to="/password/reset">パスワードの再設定へ</nuxt-link>
+    <br><br>
+    ※メールが届かない場合は、<br>
+    『no-reply@olivebodycare.healthcare』
+    <br>からのメールを受信出来るよう設定をお願い致します。
   </v-alert>
 </template>
