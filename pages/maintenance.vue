ba462358297d9c9fc7758ba7ec7e8da2e0ca1485
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-07-08 14:06:16 +0900

    fix : メンテナンスモードの画面追加

---
+++ pages/maintenance.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <v-container grid-list-xl>
+      <v-layout column wrap>
+        <v-alert :value="true" type="error" outline>
+          本日23:00~24:00の間はメンテンナス作業中でございます。<br>
+          メンテナンス完了までお待ちいただくか、<br>
+          <a href="https://olivebodycare.healthcare/about/contact">こちら</a>のお問い合わせフォームからご予約をお願いいたします。
+        </v-alert>
+      </v-layout>
+    </v-container>
+  </section>
+</template>
