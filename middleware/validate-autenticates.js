d2597eda14a79664b6b9636a3d80d8466a3e602b
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-07-14 02:14:21 +0900

    fix : 店舗IDが消えないようにする

--- middleware/validate-autenticates.js
+++ middleware/validate-autenticates.js
@@ -7,5 +7,15 @@ export default function({ store }) {
     return
   }
 
+  // undefinedのチェックをする
+  if (
+    store.state.login.accessToken === undefined ||
+    store.state.login.client === undefined ||
+    store.state.login.uid === undefined
+  ) {
+    store.commit('logout')
+    return
+  }
+
   return store.dispatch('login/validateToken')
 }
