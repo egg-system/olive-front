27d0e097831f48b1b00e5db92e66e0ecb299d5ab
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-07-16 14:37:53 +0900

    fix : redirectに修正

--- middleware/add-trailing-slash.js
+++ middleware/add-trailing-slash.js
@@ -1,9 +1,8 @@
-export default function({ app, route, query }) {
+export default function({ redirect, route, query }) {
   // 再読込時にgetパラメータが消えるため、末尾がスラッシュのurlに統一する
-  console.log(route.path)
   if (route.path.slice(-1) === '/') {
     return
   }
 
-  app.router.replace({ path: `${route.path}/`, query })
+  redirect(`${route.path}/`, query)
 }
