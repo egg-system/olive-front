f926cadca6c8d77a0ae73035fa9b36084c5ba1d0
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-07-18 13:28:15 +0900

    fix : コメントの追加

--- middleware/add-trailing-slash.js
+++ middleware/add-trailing-slash.js
@@ -1,9 +1,10 @@
 export default function({ redirect, route, query, from }) {
-  // 再読込時にgetパラメータが消えるため、末尾がスラッシュのurlに統一する
+  // リロード時にgetパラメータが消えるため、末尾がスラッシュのurlに統一する
   if (route.path.slice(-1) === '/') {
     return
   }
 
+  // 同一ルートではリダイレクト不可、かつルートの変更が検知できなくなるので、回避する
   if (from.name !== route.name) {
     redirect(`${route.path}/`, query)
   }
