cffcd3b2ade4b078528ff2c32ffb5029af6dbfd7
haji33 <hajime.sasanuma.0828@gmail.com>
2019-10-24 00:50:39 +0900

    メソッドを外だし

--- middleware/fetch-menus-from-query.js
+++ middleware/fetch-menus-from-query.js
@@ -1,9 +1,6 @@
 export default async function({ store, error, redirect, query }) {
   const { shopId, storeId, menus: _qmenus } = query
-  let qmenus
-  try {
-    qmenus = JSON.parse(_qmenus)
-  } catch (e) {}
+  const qmenus = _parseMenusQuery(_qmenus)
   if (!storeId || !Array.isArray(qmenus) || !qmenus.length) {
     redirect('/menus/')
   }
@@ -16,35 +13,42 @@ export default async function({ store, error, redirect, query }) {
   }
   const allMenus = store.getters['menu/allMenus']
   const menus = allMenus
-    .map(menu => {
-      const _menu = qmenus.find(
-        qMenu => parseInt(menu.id) === parseInt(qMenu.menuId)
-      )
-      if (!_menu) return null
-
-      const _optionIds = _menu.optionIds ? _menu.optionIds.split(',') : null
-      let options = null
-      if (
-        Array.isArray(menu.options) &&
-        menu.options.length > 0 &&
-        _optionIds
-      ) {
-        options = menu.options.filter(option =>
-          _optionIds.includes(String(option.id))
-        )
-      }
-      const mimitsuboCount = _menu.mimitsuboCount || 0
-
-      return {
-        menu,
-        options,
-        mimitsuboCount
-      }
-    })
+    .map(menu => _createMenuFromQuery(menu, qmenus))
     .filter(menu => menu)
   // 値をセット
   store.commit('reservation/select/setMenus', {
     menus,
     storeId: storeId
   })
+}
+
+function _parseMenusQuery(menusQuery) {
+  let result
+  try {
+    result = JSON.parse(menusQuery)
+  } catch (e) {}
+
+  return result
+}
+
+function _createMenuFromQuery(menu, menusQuery) {
+  const _menu = menusQuery.find(
+    qMenu => parseInt(menu.id) === parseInt(qMenu.menuId)
+  )
+  if (!_menu) return null
+
+  const _optionIds = _menu.optionIds ? _menu.optionIds.split(',') : null
+  let options = null
+  if (Array.isArray(menu.options) && menu.options.length > 0 && _optionIds) {
+    options = menu.options.filter(option =>
+      _optionIds.includes(String(option.id))
+    )
+  }
+  const mimitsuboCount = _menu.mimitsuboCount || 0
+
+  return {
+    menu,
+    options,
+    mimitsuboCount
+  }
 }
