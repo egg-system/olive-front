3510acb4efa32b4db069eb1ac20e0dc6e6d08273
gakumac <gaku.machida@gmail.com>
2020-02-07 13:06:35 +0900

    トップページの修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,92 @@
 <template>
   <section class="container">
-    <v-progress-circular
-      v-if="isProd"
-      indeterminate
-      size="100"
-    />
-    <div v-else>
-      <logo />
-      <h1 class="mainTitle">
-        olive salon
-      </h1>
-      <div class="text-xs-center">
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/mypage">
-            /mypage
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/registration">
-            /registration
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/option">
-            /option
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/menus">
-            /menus
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/login">
-            /login
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/date">
-            /date
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/confirm">
-            /confirm
-          </nuxt-link>
-        </v-chip>
-        <v-chip label text-color="white">
-          <v-icon left>
-            link
-          </v-icon>
-          <nuxt-link to="/complete">
-            /complete
-          </nuxt-link>
-        </v-chip>
-      </div>
-    </div>
+    <v-container grid-list-xl>
+      <v-layout justify-center>
+        <v-flex>
+          <v-progress-circular
+            v-if="isProd"
+            indeterminate
+            size="100"
+          />
+          <div v-else class="text-center">
+            <logo />
+            <h1 class="display-1">
+              olivebodycare
+            </h1>
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-layout justify-center>
+        <v-flex class="my-4">
+          <div>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/mypage">
+                /mypage
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/registration">
+                /registration
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/option">
+                /option
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/menus">
+                /menus
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/login">
+                /login
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/date">
+                /date
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/confirm">
+                /confirm
+              </nuxt-link>
+            </v-btn>
+            <v-btn>
+              <v-icon>
+                link
+              </v-icon>
+              <nuxt-link to="/complete">
+                /complete
+              </nuxt-link>
+            </v-btn>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </section>
 </template>
 
@@ -96,17 +106,5 @@ export default {
 }
 </script>
 
-<style scoped>
-.mainTitle {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 50px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.links {
-  padding-top: 15px;
-}
+<style lang="scss" scoped>
 </style>
